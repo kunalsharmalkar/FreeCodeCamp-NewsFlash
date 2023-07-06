0e33c7c7c9213c39b379f95b01915adf24f721db
kunalharmalkar <kunal.s.harmalkar@gmail.com>
2023-07-06 14:19:45 -0400

    41:18

--- Starter/assets/js/main.js
+++ Starter/assets/js/main.js
@@ -1,7 +1,27 @@
 // Grab elements
 
+const selectElement = selector => {
+    const element = document.querySelector(selector)
+    if(element) return element;
+    throw new Error(`Something went wrong, make sure that ${selector} exists or is typed correctly.`);
+
+};
+
+
 //Nav styles on scroll
 
+const scrollHeader = () => {
+    const headerElement = selectElement(`#header`);
+    if(this.scrollY >= 15){
+        headerElement.classList.add('activated')
+
+    }else{
+        headerElement.classList.remove('activated')
+
+    }
+};
+
+window.addEventListener('scroll', scrollHeader)
 // Open menu & search pop-up
 
 // Open/Close search form popup
